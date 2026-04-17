629e526b87b1cf97fc8331908a719f5a6dbc683e
waqas <waqas.official2005@gmail.com>
2026-04-17 22:43:40 +0500

    feat: add footer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Blog from "@/components/Blog";
 import GithubGraph from "@/components/GithubGraph";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
+import Socials from "@/components/Social";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -10,6 +12,24 @@ export default function Home() {
       <Projects />
       <Blog />
       <GithubGraph />
+      <div className="flex items-center gap-4 flex-col justify-center mb-20 text-xl">
+        <Socials />
+        <p className="text-[10px] text-neutral-600 leading-tight tracking-wider mx-auto dark:text-neutral-400">
+          Built by
+          <span className="font-bold underline decoration-neutral-300"> waqas</span>. The
+          source code is available on
+          <span>
+            <Link
+              href={"https://github.com/waqas60/sleek-portfolio"}
+              target="_blank"
+              className="font-bold underline decoration-neutral-300"
+            >
+              GitHub
+            </Link>
+            .
+          </span>
+        </p>
+      </div>
     </div>
   );
 }
--- src/components/GithubGraph.tsx
+++ src/components/GithubGraph.tsx
@@ -1,12 +1,15 @@
 import { GitHubCalendar } from "react-github-calendar";
 
 export default function GithubGraph() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <section className="">
       <h1 className="text-md font-semibold mb-4">Github</h1>
       <div className="overflow-x-auto md:overflow-hidden flex justify-center w-full rounded-2xl border border-neutral-200 p-4 dark:border-neutral-600 select-none">
         <GitHubCalendar
           username="waqas60"
+          year={currentYear}
           blockSize={8}
           blockMargin={3}
           fontSize={10}
