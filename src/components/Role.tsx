12bee674d2a57504072f15773f25613c7e9b17c9
waqas <waqas.official2005@gmail.com>
2026-04-18 16:57:36 +0500

    fix: blog coming soo

--- src/components/Role.tsx
+++ src/components/Role.tsx
@@ -2,12 +2,12 @@ const headings = ["Full Stack Developer", "Frontend Engineer"];
 
 export default function Role() {
   return (
-    <div className="overflow-hidden items-center flex ">
+    <div className="relative h-7 w-48">
       {headings.map((text, i) => (
         <h1
           key={i}
-          className="animate-textCycle text-xs border border-neutral-300 shadow-md dark:text-neutral-200 text-neutral-500 p-1 rounded-md"
-          style={{ animationDelay: `${i * (10 / headings.length)}s` }}
+          className="animate-textCycle absolute text-[10px] border border-neutral-300 shadow-xs dark:text-neutral-200 text-neutral-500 p-1 rounded-md whitespace-nowrap"
+          style={{ animationDelay: `${i * (6 / headings.length)}s` }}
         >
           {text}
         </h1>
--- src/data/blogs.tsx
+++ src/data/blogs.tsx
@@ -2,11 +2,10 @@ import { BlogType } from "@/types/blogs";
 
 const blogs: BlogType[] = [
   {
-    title: "Hello World in Rust with Axum",
-    description:
-      "A minimal Hello World web server using Rust and the Axum framework.",
-    slug: "hello-world-in-rust-with-axum",
-    date: "Jan 03, 2026",
+    title: "Coming Soon",
+    description: "Coming Soon",
+    slug: "blog-coming-soon",
+    date: "April 18, 2026",
   },
 ];
 
